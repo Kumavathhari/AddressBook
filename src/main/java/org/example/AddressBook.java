f350447575707ce5973a8224920ef053ab1bef4a
Hari Naik <kumavathharinaik@gmail.com>
2024-06-21 23:15:45 +0530

    Adding Contact details

--- src/main/java/org/example/AddressBook.java
+++ src/main/java/org/example/AddressBook.java
@@ -1,8 +1,33 @@
 package org.example;
-
+import java.util.ArrayList;
+import java.util.List;
 public class AddressBook {
+        private List<Contact> contacts;
+
+        public AddressBook() {
+            this.contacts = new ArrayList<>();
+        }
+
+        public void addContact(Contact contact) {
+            contacts.add(contact);
+        }
+
+        public List<Contact> getContacts() {
+            return contacts;
+        }
+
         public static void main(String[] args) {
-            System.out.println("Welcome to Address Book Program");
+            AddressBook addressBook = new AddressBook();
+
+            Contact contact1 = new Contact("Hari", "Naik", "123 Elm St", "Mumbai", "MH", "517000", "1234567890", "HariNaik@example.com");
+            Contact contact2 = new Contact("Prince", "Haru", "456 Maple Ave", "Bangolore", "KA", "513775", "1213457897", "PrinceHaru@example.com");
+
+            addressBook.addContact(contact1);
+            addressBook.addContact(contact2);
+
+            for (Contact contact : addressBook.getContacts()) {
+                System.out.println(contact);
+            }
         }
     }
 
